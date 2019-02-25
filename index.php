2e1f36daebb5b6387675ecbac5738a8ffa02da6a
Laysson Enderson <laysson_enderson@hotmail.com>
2019-02-24 21:47:58 -0300

    catalogo

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <?php
-
+//Declaração variavel
 $var = 'texto 1';
 $var2 = 'texto 2';
+
+//Declaração array
+$lista_compras[1] = 'Teste 1';
+$lista_compras[2] = 'Teste 2';
+$lista_compras[3] = 'Teste 3';
+$lista_compras[4] = 'Teste 4';
+$lista_compras[5] = 'Teste 5';
+
 
 //este codigo serve para impressão
 echo 'TESTANDO <br>';
 echo $var . $var2;//concatenando
 
- 
+// array
+var_dump($lista_compras);
+
+print_r($lista_compras);
  ?>
