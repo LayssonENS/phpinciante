521b83106cea40c0794d986b75d9f6fa4bb7efc6
Laysson Enderson <laysson_enderson@hotmail.com>
2019-02-28 11:48:15 -0300

    funções basicas

--- funcoes_basicas.php
+++ funcoes_basicas.php
@@ -1,5 +1,5 @@
 <?php
- //isset restorna true caro a variavel tenha sido inicializada ou nula
+ //isset restorna true caso a variavel tenha sido inicializada ou nula
 $valor = 'Laysson';
 if (isset($valor)) {
     echo 'Variavel iniciada <br>';
@@ -7,13 +7,13 @@ if (isset($valor)) {
 
 //empty retorna true sempre que a variavel estiver vazia ou = 0 ou nulo
 $valor2 = '';
-if (isset($valor2)) {
-    echo 'Variavel iniciada <br>';
+if (empty($valor2)) {
+    echo 'Variavel vazia <br>';
 }
 
 //is_numeric testa se a variável é numero ou String, retorna true caso for numero, mesmto colocando como string
 $valor3 = 3;
 if (is_numeric($valor3)) {
-    echo 'Variavel iniciada <br>';
+    echo 'Variavel é um numero <br>';
 }
  ?>
